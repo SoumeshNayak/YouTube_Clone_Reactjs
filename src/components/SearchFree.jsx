3d1d45f1b12fd855c81e7a47fd97f6b1ffbaa28b
Soumesh Nayak <soumesh.nayak@gyansys.com>
2024-08-29 15:21:36 +0530

    added search functionality

--- src/components/SearchFree.jsx
+++ src/components/SearchFree.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Box,Typography} from "@mui/material"
 
-const SearchFree = () => {
+import Videos from './Videos'
+import { fetchFromApi } from '../utils/fetchFromApi'
+import { useParams } from 'react-router-dom'
+const SearchFree = () =>{
+ 
+  const [videos,setVideos]=useState([])
+  const { searchTerm } = useParams();
+
+
+  useEffect(()=>{
+      fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data)=>{setVideos(data.items)})
+  },[])
+
+
   return (
-    <div>SearchFree</div>
-  )
+  <Box p={2} minHeight="95vh">
+    <Typography variant="h4" fontWeight={900} mb={3} color= "white" ml={{ sm: "100px"}}>
+    Search Results for<span style={{
+        color:"#F31503"
+      }}>{searchTerm}</span>videos
+    </Typography>
+    <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}>
+        {<Videos videos={videos} />}
+        </Box>
+    </Box>  
+    
+    </Box>
+    )
 }
 
 export default SearchFree
